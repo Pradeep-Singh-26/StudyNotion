c9c927f5aaeb36be85a8197bdfffab7fcf24a6ff
Pradeep-Singh-26 <pradeepgit26@gmail.com>
2026-06-25 13:27:29 +0530

    mailsender fix 2

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -3,13 +3,15 @@ const nodemailer = require("nodemailer");
 const mailSender = async (email, title, body) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: "gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
             auth: {
                 user: process.env.MAIL_USER,
                 pass: process.env.MAIL_PASS,
             },
+            connectionTimeout: 30000,
         });
-
         let info = await transporter.sendMail({
             from: `StudyNotion <${process.env.MAIL_USER}>`,
             to: `${email}`,
